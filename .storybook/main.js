e34032d728c9891a12a01babc351ce486d6beb2d
yk-mt12 <agvksl543@gmail.com>
2023-02-05 18:39:29 +0900

    storybookを設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,35 +1,24 @@
-const TsconfigPathPlugin = require('tsconfig-paths-webpack-plugin')
-const path = require('path')
-
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
+  staticDirs: ['public'],
   babel: async options => ({
     ...options,
-    plugins: [
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-private-methods',
-      '@babel/plugin-proposal-private-property-in-object',
-    ],
+    plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-private-methods', '@babel/plugin-proposal-private-property-in-object']
   }),
-  staticDirs: ['public'],
-  webpackFinal: async (config) => {
-    config.resolve.plugins = [
-      new TsconfigPathPlugin({
-        configFile: path.resolve(__dirname, '../tsconfig.json')
-      }),
-    ];
-    return config
+  webpackFinal: async config => {
+    config.resolve.plugins = [new TsconfigPathsPlugin({
+      configFile: path.resolve(__dirname, '../tsconfig.json')
+    })];
+    return config;
   },
-}
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
-import { addDecorator } from "@storybook/react"
-import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { addDecorator } from '@storybook/react'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import { theme } from '../src/themes'
 import * as NextImage from 'next/image'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -19,23 +19,22 @@ export const GlobalStyle = createGlobalStyle`
   textarea {
     padding: 0;
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Robot, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
-
   * {
     box-sizing: border-box;
   }
-
   a {
     text-decoration: none;
     transition: .25s;
-    color: #000;
+    color: #000000;
   }
 `
 
 // Themeの適用
 addDecorator((story) => (
-  <ThemeProvider them={theme} >
+  <ThemeProvider theme={theme}>
     <GlobalStyle />
     {story()}
   </ThemeProvider>
@@ -43,6 +42,7 @@ addDecorator((story) => (
 
 // next/imageの差し替え
 const OriginalNextImage = NextImage.default;
+
 Object.defineProperty(NextImage, 'default', {
   configurable: true,
   value: (props) => typeof props.src === 'string' ? (
@@ -51,9 +51,3 @@ Object.defineProperty(NextImage, 'default', {
     <OriginalNextImage {...props} unoptimized />
   ),
 })
-
-Object.defineProperty(NextImage, '__esModule', {
-  configurable: true,
-  value: true
-})
-
