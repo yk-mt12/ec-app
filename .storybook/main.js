c382b27920b46bc4ccc23aca1e0a8493f31c0c8b
yk-mt12 <agvksl543@gmail.com>
2023-02-05 12:38:28 +0900

    test: setup storybook

---
+++ .storybook/main.js
@@ -0,0 +1,35 @@
+const TsconfigPathPlugin = require('tsconfig-paths-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
+  babel: async options => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  }),
+  staticDirs: ['public'],
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json')
+      }),
+    ];
+    return config
+  },
+}
